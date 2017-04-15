5978ab8e164147c408d05ac8ab7d0e8335358f78
tristanto <tristanto.kurniawan@gmail.com>
2017-04-15 07:40:50 +0700

    Fix blueprint naming (for url_for)

--- app/blueprints/home_blueprint.py
+++ app/blueprints/home_blueprint.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, abort, render_template
 
 
-home_blueprint = Blueprint('home_page', __name__, template_folder='templates')
+home_blueprint = Blueprint('home', __name__, template_folder='templates')
 
 
 @home_blueprint.route('/')
