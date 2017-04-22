d95f2505e47385ff8f612d5752ae1f691d150169
tristanto <tristanto.kurniawan@gmail.com>
2017-04-22 23:24:45 +0700

    Use hook before request instead condition in blueprint

--- app/blueprints/home_blueprint.py
+++ app/blueprints/home_blueprint.py
@@ -5,14 +5,16 @@ from app.forms.simple_form import SimpleForm
 home_blueprint = Blueprint('home', __name__, template_folder='templates')
 
 
-@home_blueprint.route('/', methods=['POST', 'GET'])
-def index():
+@home_blueprint.before_request
+def before_request():
     form = SimpleForm()
     if form.validate_on_submit():
         session['name'] = form.name.data
         return redirect(url_for('home.index'))
-    else:
-        try:
-            return render_template('home/index.html', form=form, name=session.get('name'))
-        except TemplateNotFound:
-            abort(404)
+
+@home_blueprint.route('/', methods=['POST', 'GET'])
+def index():
+    try:
+        return render_template('home/index.html', form=SimpleForm(), name=session.get('name'))
+    except TemplateNotFound:
+        abort(404)
